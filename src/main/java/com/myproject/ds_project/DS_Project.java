598837695342d152fbc7b9870068dbef0b77c3b1
Loly2020 <looloos321@hotmail.com>
2026-05-06 20:02:53 +0300

    added

---
+++ src/main/java/com/myproject/ds_project/DS_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myproject.ds_project;
+
+/**
+ *
+ * @author loolo
+ */
+public class DS_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
