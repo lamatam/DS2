e9150e50c74656cae4083d02043481ecb69ca40b
queenOfIs <alyahyasara1426@gmail.com>
2026-05-13 05:24:17 +0300

    Update DS_Project.java

--- src/main/java/com/myproject/ds_project/DS_Project.java
+++ src/main/java/com/myproject/ds_project/DS_Project.java
@@ -11,6 +11,36 @@ package com.myproject.ds_project;
 public class DS_Project {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //menu
+        while (true) {
+
+    System.out.println("\nWelcome to the Student Advising System!");
+    System.out.println("\nPlease choose an option:");
+
+    System.out.println("0. Load data from files");
+    System.out.println("1. Add a student");
+    System.out.println("2. Search for a student");
+    System.out.println("3. Delete a student");
+    System.out.println("4. Schedule a meeting/workshop");
+    System.out.println("5. Print meeting/workshop details");
+    System.out.println("6. Print students by name");
+    System.out.println("7. Print all meetings/workshops alphabetically");
+    System.out.println("8. Exit");
+
+    System.out.println("9. Print students by major");
+    System.out.println("10. Print students by year level");
+    System.out.println("11. Print students by partial name");
+    System.out.println("12. Print workshop students");
+    System.out.println("13. Search student by email");
+    System.out.println("14. Print all students");
+    System.out.println("15. Print event details by title");
+    System.out.println("16. Print event details by student name");
+
+    System.out.print("\nEnter your choice: ");
+
+    int choice = input.nextInt();
+    input.nextLine();
+}
     }
 }
