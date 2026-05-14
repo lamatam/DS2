ef43ac31f0d93cd5ead7f3bc4a7df3e0648c1f91
Maymonah-IS <lonely.maymoon@gmail.com>
2026-05-15 02:24:59 +0300

    Update Event.java

--- src/main/java/com/myproject/ds_project/Event.java
+++ src/main/java/com/myproject/ds_project/Event.java
@@ -8,51 +8,91 @@ package com.myproject.ds_project;
  *
  * @author loolo
  */
-public class Event implements IEvent{
-
+public abstract class Event implements IEvent{
+    //attributes:
+    int eventId;
+    String title;
+    IDateTime startDateTime;
+    IDateTime endDateTime;
+    String location;
+    
     @Override
     public int getEventId() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return eventId;
     }
 
     @Override
     public String getTitle() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return title;
     }
 
     @Override
     public void setTitle(String title) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        this.title = title;
     }
 
     @Override
     public IDateTime getStartDateTime() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return startDateTime;
     }
 
     @Override
     public IDateTime getEndDateTime() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return endDateTime;
     }
 
     @Override
     public String getLocation() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return location;
     }
 
     @Override
     public void setLocation(String location) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        this.location = location;
     }
 
     @Override
-    public boolean hasStudent(int studentId) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public boolean hasStudent(int studentId) { //check if student is in this event
+        return false;//overriden by its children
     }
-
+    
+    @Override
+    public String toString(){
+        String str;
+        str = "Event title :" + title + "\n Event ID: " + eventId + "\n Start date and time: " +
+                startDateTime+ "\n End date and time: " + endDateTime + "\n Location: " + location;
+        return str;
+    }
     @Override
     public int compareTo(IEvent other) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if(other.getTitle() == null)
+            throw new NullPointerException("The other event cannot be null");
+        int len1 = this.title.length();
+        int len2 = other.getTitle().length();
+        int minLen;
+        if(len1>len2)
+            minLen = len2;
+        else
+            minLen = len1;
+        
+        char c1,c2;
+        for(int i=0; i<minLen; i++){
+            c1 = this.title.charAt(i);
+            c2 = other.getTitle().charAt(i);
+            
+            if(c1>c2)
+                return 1;
+            if(c1<c2)
+                return -1;
+        }
+        
+        if(len1<len2)
+            return -1;
+        if(len1>len2)
+            return 1;
+    
+        return 0;// in this case the titles are identical
+                
     }
     
 }
