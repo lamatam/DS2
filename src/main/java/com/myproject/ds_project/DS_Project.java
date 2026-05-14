79a2270b9b4ad43631d90a9b12efab0a504f54f0
lamatam <lama.tamraaan@gmail.com>
2026-05-14 19:49:47 +0300

    Update DS_Project.java
    statements

--- src/main/java/com/myproject/ds_project/DS_Project.java
+++ src/main/java/com/myproject/ds_project/DS_Project.java
@@ -18,7 +18,7 @@ public class DS_Project {
     System.out.println("\nWelcome to the Student Advising System!");
     System.out.println("\nPlease choose an option:");
 
-    System.out.println("0. Load data from files");
+    //System.out.println("0. Load data from files");
     System.out.println("1. Add a student");
     System.out.println("2. Search for a student");
     System.out.println("3. Delete a student");
@@ -26,16 +26,18 @@ public class DS_Project {
     System.out.println("5. Print meeting/workshop details");
     System.out.println("6. Print students by name");
     System.out.println("7. Print all meetings/workshops alphabetically");
-    System.out.println("8. Exit");
+    System.out.println("8. Print all workshops certain TITLE alphabetically");
+    System.out.println("9. Print students ordered by ID");
+    System.out.println("10. Exit");
 
-    System.out.println("9. Print students by major");
-    System.out.println("10. Print students by year level");
-    System.out.println("11. Print students by partial name");
-    System.out.println("12. Print workshop students");
-    System.out.println("13. Search student by email");
-    System.out.println("14. Print all students");
-    System.out.println("15. Print event details by title");
-    System.out.println("16. Print event details by student name");
+   // System.out.println("9. Print students by major");
+   //System.out.println("10. Print students by year level");
+    //System.out.println("11. Print students by partial name");
+    //System.out.println("12. Print workshop students");
+    //System.out.println("13. Search student by email");
+    //System.out.println("14. Print all students");
+    //System.out.println("15. Print event details by title");
+    //System.out.println("16. Print event details by student name");
 
     System.out.print("\nEnter your choice: ");
 
