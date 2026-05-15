370484c49d086a52c5605cb6cbc4afab0bdfabff
Maymonah-IS <lonely.maymoon@gmail.com>
2026-05-15 04:00:50 +0300

    Update Workshop.java

--- src/main/java/com/myproject/ds_project/Workshop.java
+++ src/main/java/com/myproject/ds_project/Workshop.java
@@ -8,131 +8,97 @@ package com.myproject.ds_project;
  *
  * @author loolo
  */
-public class Workshop implements IWorkshop{
+public class Workshop extends Event implements IWorkshop {
+
     public IStudentList students;
-    final int EventId;
-    String title;
-    IDateTime startDateTime;
-    IDateTime endDateTime;
-    String location;
-    
-    public Workshop(int eID, IDateTime sDT , IDateTime eDT )
-        {
-            students = new StudentList();
-            EventId = eID;
-            startDateTime = new DateTime (sDT);
-            endDateTime = new DateTime (eDT);
-            
-        }
-    public Workshop(int eID, String t, IDateTime sDT , IDateTime eDT, String loc )
-        {
-            EventId = eID;
-            title = t;
-            startDateTime = new DateTime (sDT);
-            endDateTime = new DateTime (eDT);
-            location = loc;
-            
-            students = new StudentList();
-        }
-    public Workshop(int eID, String t, IDateTime sDT , IDateTime eDT, String loc, IStudent std )
-        {
-            EventId = eID;
-            title = t;
-            startDateTime = new DateTime (sDT);
-            endDateTime = new DateTime (eDT);
-            location = loc;
 
-            students = new StudentList();
-            students.add(std);
-        }
-    public Workshop(Workshop workshop )
-        {
-            EventId = workshop.EventId;
-            title = workshop.title;
-            startDateTime = new DateTime (workshop.startDateTime);
-            endDateTime = new DateTime (workshop.endDateTime);
-            location = workshop.location;
+    public Workshop(int eID, IDateTime sDT, IDateTime eDT) {
+        students = new StudentList();
+        eventId = eID;
+        startDateTime = new DateTime(sDT);
+        endDateTime = new DateTime(eDT);
+    }
 
-            students = new StudentList();
-            if (! workshop.getParticipants().empty())
-            {
-                workshop.getParticipants().findFirst();
-                while ( ! workshop.getParticipants().last())
-                {
-                    students.add(workshop.getParticipants().retrieve());
-                    workshop.getParticipants().findNext();
-                }
+    public Workshop(int eID, String t, IDateTime sDT, IDateTime eDT, String loc) {
+        eventId = eID;
+        title = t;
+        startDateTime = new DateTime(sDT);
+        endDateTime = new DateTime(eDT);
+        location = loc;
+        students = new StudentList();
+    }
+
+    public Workshop(int eID, String t, IDateTime sDT, IDateTime eDT, String loc, IStudent std) {
+        eventId = eID;
+        title = t;
+        startDateTime = new DateTime(sDT);
+        endDateTime = new DateTime(eDT);
+        location = loc;
+        students = new StudentList();
+        students.add(std);
+    }
+
+    public Workshop(Workshop workshop) {
+        eventId = workshop.eventId;
+        title = workshop.title;
+        startDateTime = new DateTime(workshop.startDateTime);
+        endDateTime = new DateTime(workshop.endDateTime);
+        location = workshop.location;
+        students = new StudentList();
+        if (!workshop.getParticipants().empty()) {
+            workshop.getParticipants().findFirst();
+            while (!workshop.getParticipants().last()) {
                 students.add(workshop.getParticipants().retrieve());
+                workshop.getParticipants().findNext();
             }
+            students.add(workshop.getParticipants().retrieve());
         }
+    }
 
     @Override
     public LinkedList<IStudent> getParticipants() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return (LinkedList<IStudent>) students;//casting 
     }
 
     @Override
     public boolean addParticipant(IStudent student) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (hasStudent(student.getStudentId())) {//to insure there is no duplicates
+            return false;
+        }
+        students.add(student);
+        return true;
     }
 
     @Override
     public boolean removeParticipantById(int studentId) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (!hasStudent(studentId)) {
+            return false; //kk in main put System.out.println("There is no such a student participated in this workshop");
+        }
+        students.deleteById(studentId);
+        return true;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public int getEventId() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public String getTitle() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void setTitle(String title) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public IDateTime getStartDateTime() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public IDateTime getEndDateTime() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public String getLocation() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void setLocation(String location) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return getParticipants().empty();//reuse
     }
 
     @Override
     public boolean hasStudent(int studentId) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (isEmpty()) {
+            return false;
+        }
+        LinkedList tmp = students.getAll();
+        tmp.findFirst();
+        for (int i = 0; i < tmp.size; i++) {
+            if (tmp.retrieve() != null && ((Student) (tmp.retrieve())).getStudentId() == studentId) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    @Override
-    public int compareTo(IEvent other) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public String getEventType() {
+        return "Workshop";
     }
-//
-    @Override
-public String getEventType() {
-    return "Workshop";
-}
 }
