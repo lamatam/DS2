3eab75b13fafe1233a263e2affcd3b35cc3465f2
lamatam <lama.tamraaan@gmail.com>
2026-05-16 01:18:58 +0300

    Update DateTime.java

--- src/main/java/com/myproject/ds_project/DateTime.java
+++ src/main/java/com/myproject/ds_project/DateTime.java
@@ -16,20 +16,20 @@ public class DateTime implements IDateTime {
     int minute;
     public DateTime (String dt)
     {
-    date = date.trim();
-        int firstSlash = date.indexOf('/');
-        int secondSlash  = date.lastIndexOf('/');
-        int space = date.lastIndexOf(' ');
-        int first_dots = date.indexOf(':');
+        dt = dt.trim();
+        int first_S  = dt.indexOf('/');
+        int second_S  = dt.lastIndexOf('/');
+        int space = dt.lastIndexOf(' ');
+        int first_dots = dt.indexOf(':');
         
-        this.month = Integer.parseInt(date.substring(0, firstSlash ));
-        this.day = Integer.parseInt(date.substring( firstSlash +1, secondSlash));
-        this.year = Integer.parseInt(date.substring(secondSlash+1, space));
-        this.hour = Integer.parseInt(date.substring( space+1 , first_dots));
-        this.minute = Integer.parseInt(date.substring( first_dots+1));
+        this.month = Integer.parseInt(dt.substring(0,first_S));
+        this.day = Integer.parseInt(dt.substring(first_S+1, second_S));
+        this.year = Integer.parseInt(dt.substring( second_S+1, space));
+        this.hour = Integer.parseInt(dt.substring( space+1 , first_dots));
+        this.minute = Integer.parseInt(dt.substring( first_dots+1));
         
     }
-    public DateTime ( int year, int month, int day , int hour, int minute)
+    public DateTime ( int year, int month, int day , int hour, int mintue)
     {
         this.year = year;
         this.month = month;
