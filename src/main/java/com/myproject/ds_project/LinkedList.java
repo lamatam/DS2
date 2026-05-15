4437631a9621d3541beb4f401f676d1a6c9cfb34
Loly2020 <looloos321@hotmail.com>
2026-05-15 15:22:41 +0300

    ؤخةةهف

--- src/main/java/com/myproject/ds_project/LinkedList.java
+++ src/main/java/com/myproject/ds_project/LinkedList.java
@@ -47,18 +47,19 @@ public class LinkedList<T> implements List<T> {
 		current.data = val;
 	}
 	
-	public void insert (T val) {
-		Node<T> tmp;
-		if (empty()) {
-			current = head = new Node<T> (val);
-		}
-		else {
-			tmp = current.next;
-			current.next = new Node<T> (val);
-			current = current.next;
-			current.next = tmp;
-		}
-	}
+    public void insert (T val) {
+            Node<T> tmp;
+            if (empty()) {
+                    current = head = new Node<T> (val);
+            }
+            else {
+                    tmp = current.next;
+                    current.next = new Node<T> (val);
+                    current = current.next;
+                    current.next = tmp;
+            }
+            size ++;
+    }
         ////////////++++++++++++++++++++++++++++
             // add before first node 
     public void insertAtBegin (T val) {
@@ -94,21 +95,22 @@ public class LinkedList<T> implements List<T> {
             size ++;
     }  ////////////////////////++++++++++++++++++++++++++++++
 	
-	public void remove () {
-		if (current == head) {
-			head = head.next;
-		}
-		else {
-			Node<T> tmp = head;
-			while (tmp.next != current)
-				tmp = tmp.next;
-			tmp.next = current.next;
-		}
-		if (current.next == null)
-			current = head;
-		else
-			current = current.next;
-	}
+    public void remove () {
+            if (current == head) {
+                    head = head.next;
+            }
+            else {
+                    Node<T> tmp = head;
+                    while (tmp.next != current)
+                            tmp = tmp.next;
+                    tmp.next = current.next;
+            }
+            if (current.next == null)
+                    current = head;
+            else
+                    current = current.next;
+            size --;
+    }
 
     public int getSize()
     {
