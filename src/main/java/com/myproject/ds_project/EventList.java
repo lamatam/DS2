eb65315e7fe363d2dd494b811fa656c86acf4439
Maymonah-IS <lonely.maymoon@gmail.com>
2026-05-15 08:17:26 +0300

    Update EventList.java

--- src/main/java/com/myproject/ds_project/EventList.java
+++ src/main/java/com/myproject/ds_project/EventList.java
@@ -8,36 +8,97 @@ package com.myproject.ds_project;
  *
  * @author loolo
  */
-public class EventList implements IEventList{
+public class EventList implements IEventList {
+
+    public LinkedList<IEvent> events = new LinkedList<IEvent>();
 
     @Override
     public boolean addEvent(IEvent event) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (events.empty()) {
+            events.insert(event);
+            return true;
+        }
+        events.findFirst();
+        if ((event.getTitle()).compareTo(events.retrieve().getTitle()) < 0) {
+            events.insertAtBegin(event);
+            return true;
+        }
+        for (int i = 0; i < events.size; i++) {
+            if ((event.getTitle()).compareTo(events.retrieve().getTitle()) >= 0) {
+                events.findNext(); //move current to insert the new event before it
+                events.insertBefore(event);
+                return true;
+            }
+            events.findNext();//move current for next iteration
+        }
+        return false;
     }
 
     @Override
     public boolean removeEventById(int eventId) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (events == null) {
+            return false;
+        }
+        events.findFirst();// move current to start
+        for (int i = 0; i < events.size; i++) {
+            if (events.retrieve().getEventId() == eventId) {
+                events.remove();
+                return true;
+            }
+            events.findNext();//to move current (prevent meaningless loop)
+        }
+        return false;
     }
 
     @Override
     public LinkedList<IEvent> getAllAlphabetically() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return events;
     }
 
     @Override
     public LinkedList<IEvent> findByTitle(String title) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (events.empty()) {
+            return null;
+        }
+        events.findFirst();// move current to start
+        LinkedList<IEvent> matchingEvents = new LinkedList<IEvent>();
+        for (int i = 0; i < events.size; i++) {
+            if (title.compareTo(events.retrieve().getTitle()) == 0) {
+                matchingEvents.insert(events.retrieve());
+            }
+        }
+        events.findNext();//to move current (prevent meaningless loop)
+        return matchingEvents;
     }
 
     @Override
     public LinkedList<IEvent> findByStudentName(String studentFullName) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        events.findFirst();// move current to start
+        LinkedList<IEvent> matchingEvents = new LinkedList<IEvent>();
+        LinkedList<IStudent> studentse = null;
+        for (int i = 0; i < events.size; i++) {
+            if (events.retrieve() instanceof Workshop workshop) {
+                studentse = (workshop.getParticipants());
+            }
+            if (events.retrieve() instanceof Meeting meeting) {
+                studentse.insert(meeting.getStudent());
+            }
+            for (int j = 0; j < studentse.size; j++) {
+                if (studentse.retrieve().getName().equals(studentFullName)) {
+                    matchingEvents.insert(events.retrieve());
+                    events.findNext();
+                    break;
+                }
+                studentse.findNext();
+            }
+            events.findNext();
+        }
+        return matchingEvents;
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return events.size;
     }
-    
+
 }
